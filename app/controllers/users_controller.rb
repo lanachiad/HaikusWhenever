d7945d1e34a64ac022b1b0acc5c656120281284f
Lana Chiad <lanachiad7@gmail.com>
2017-06-26 21:48:11 -0400

    Add remaining controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def login
+  end
+
+  def loggingin
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
