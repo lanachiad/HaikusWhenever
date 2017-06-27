bfe99a6103bcd1c4d26440e3bd016f996f50a528
Lana Chiad <lanachiad7@gmail.com>
2017-06-26 22:01:24 -0400

    Create routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/users/login', to: 'users#login'
+  get '/users/loggingin', to: 'users#loggingin'
+
   resources :users, only: [:show, :create, :new, :edit, :delete] do 
     resources :haikus 
   end
