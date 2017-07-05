1b211c46a17aae479efbb8649f99d8530d3ffb3f
Lana Chiad <lanachiad7@gmail.com>
2017-07-05 14:12:09 -0400

    Update seeds to include title, first second and third lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 user = User.create(first_name: "Lana", last_name: "Chiad", username: "lanana", password: "1234")
 
-haiku = Haiku.create(poem: "If my career fails, I can get a new career, Building furniture.", user_id: 1)
+haiku = Haiku.create(title: "IKEA", first_line: "If my career fails,", second_line: "I can get a new career,", third_line: "Building furniture.", user_id: 1)
