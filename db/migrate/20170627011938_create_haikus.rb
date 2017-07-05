06a89622f950cec307fe727b8316cdef670b11c1
Lana Chiad <lanachiad7@gmail.com>
2017-07-05 14:11:53 -0400

    Create first, second, and third lines of haiku, as well as title

--- db/migrate/20170627011938_create_haikus.rb
+++ db/migrate/20170627011938_create_haikus.rb
@@ -1,7 +1,10 @@
 class CreateHaikus < ActiveRecord::Migration[5.1]
   def change
     create_table :haikus do |t|
-      t.text :poem
+      t.string :title
+      t.text :first_line
+      t.text :second_line
+      t.text :third_line
       t.references :user, foreign_key: true, null: false
 
       t.timestamps
