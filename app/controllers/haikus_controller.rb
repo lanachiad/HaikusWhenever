2b09f7f39440ecce04aea78066ce83b4ff4bc455
Lana Chiad <lanachiad7@gmail.com>
2017-07-06 18:59:41 -0400

    add new and create controller methods

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -4,9 +4,18 @@ class HaikusController < ApplicationController
   end
 
   def new
+    @haiku = Haiku.new
   end
 
   def create
+    @haiku = current_user.haikus.new(haiku_params)
+
+    if @haiku.save
+      redirect_to haikus_path
+    else
+      @errors = @haiku.errors.full_messages
+      render 'new'
+    end
   end
 
   def edit
@@ -16,5 +25,10 @@ class HaikusController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def haiku_params
+    params.require(:haiku).permit(:title,:first_line,:second_line, :third_line, :user_id)
   end
 end
